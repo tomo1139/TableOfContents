27f1d2e49934daf19c732880cee43961695b9273
beta1139 <beta1139@gmail.com>
2018-09-21 22:33:16 +0900

    databinding 有効化

--- TableOfContents/app/src/main/java/com/example/tomo/tableofcontents/MainActivity.kt
+++ TableOfContents/app/src/main/java/com/example/tomo/tableofcontents/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.tomo.tableofcontents
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tomo.tableofcontents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
 }
